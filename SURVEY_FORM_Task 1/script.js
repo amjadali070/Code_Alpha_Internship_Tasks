064352cc7561b0474e82deb0d79763f2314a5181
amjadali070 <amjadpitafi070@gmail.com>
2023-08-05 00:57:00 +0500

    code updated

--- SURVEY_FORM_Task 1/script.js
+++ SURVEY_FORM_Task 1/script.js
@@ -1,26 +1,63 @@
 const form = document.getElementById('survey-form');
 const pages = document.querySelectorAll('.page');
-let currentPage = 1;
+const formData = {};
 
 function showPage(pageNumber) {
   pages.forEach(page => page.classList.remove('active'));
   pages[pageNumber - 1].classList.add('active');
-  currentPage = pageNumber;
 }
 
 function nextPage(pageNumber) {
-  saveFormData(); // Implement this function to save form data to local storage or variables
+  saveFormData();
   showPage(pageNumber);
+  restoreFormData();
 }
 
 function prevPage(pageNumber) {
-  saveFormData(); // Implement this function to save form data to local storage or variables
+  saveFormData();
   showPage(pageNumber);
+  restoreFormData();
+}
+
+function saveFormData() {
+  const currentPage = pages[getCurrentPageNumber() - 1];
+  const inputs = currentPage.querySelectorAll('.form-control');
+  
+  inputs.forEach(input => {
+    formData[input.id] = input.value;
+  });
+  
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
+
+function restoreFormData() {
+  const savedData = JSON.parse(localStorage.getItem('formData'));
+  
+  if (savedData) {
+    const currentPage = pages[getCurrentPageNumber() - 1];
+    const inputs = currentPage.querySelectorAll('.form-control');
+  
+    inputs.forEach(input => {
+      if (savedData[input.id]) {
+        input.value = savedData[input.id];
+      }
+    });
+  }
+}
+
+function getCurrentPageNumber() {
+  for (let i = 0; i < pages.length; i++) {
+    if (pages[i].classList.contains('active')) {
+      return i + 1;
+    }
+  }
+  return 1; // Default to first page
 }
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-  submitForm(); // Implement this function to handle form submission
+  submitForm();
 });
 
-showPage(currentPage);
+showPage(1);
+restoreFormData();
